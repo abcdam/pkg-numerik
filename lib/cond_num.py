f3d085d11f775839bfe24ddb0ddf7211a613afc6
illikev1 <illikev1@students.zhaw.ch>
2021-01-24 10:05:26 +0100

    moved jupiter notebooks + update cond_num.py

--- lib/cond_num.py
+++ lib/cond_num.py
@@ -1,14 +1,35 @@
-from scipy.misc import derivative
 import numpy as np
+import matplotlib.pyplot as plt
 
 # condition is calculated with the following formula: 
 #
-#      |f'(x0)|*|x0|
+#      |f'(x)|*|x|
 # K := -------------
 #         |f(x)|
 #
 # condition K is small -> well conditioned problem
 # condition K is big   -> badly conditioned problem
 #
-def calculate_cond(function, x0, step=1, nth_derivative=1):
-    return np.abs(derivative(function, x0, step, nth_derivative),dtype='float64')*np.abs(x0,dtype='float64')/np.abs(function(x0),dtype='float64')
+
+def function(x):
+    return x*np.exp(x)
+def derivative(x):
+    return x*np.exp(x)+np.exp(x)
+def condition(value):
+    return (np.abs(derivative(value))*np.abs(value))/np.abs(function(value))
+
+# plot
+
+step = 0.00001
+a = -2
+b = 2
+plotting_range = (a,b+step)
+x_values = np.arange(plotting_range[0],plotting_range[1],step)
+y_values = [condition(x) for x in x_values]
+plt.plot(x_values, y_values, label='K in relation to x')
+plt.xlabel('x values')
+plt.ylabel('Condition K')
+plt.legend()
+plt.show() # not needed in jupyter
+
+
